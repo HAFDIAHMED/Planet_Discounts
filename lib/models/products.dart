17ebdffb01d30c125f3967ea0c7895f9a7a5390b
HAFDIAHMED <ahafdi62@gmail.com>
2020-11-08 20:58:12 +0100

    new changes

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -25,7 +25,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/1602199935Tvis5liJ.png',
       price: 249,
-      category: 'Manucure',
+      category: 'sac',
       description:
           'nspiration:Le royaume du blanc et du grand bleu. Abrite de puissants ours polaires, de fiers lions de mer et d adorables phoques. De vastes étendues de l Arctique impénétrable et solitaire attendent d être découvertes. Le blanc pur de la neige et de la glace non polluées gèlera tous vos amis! Caractéristique:Blanche neige, parfaite pour la décoration "française".Couverture de couleur - complète.',
     ),
@@ -35,7 +35,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/1602188594yUlXVqnN.png',
       price: 199,
-      category: 'Manucure',
+      category: 'Pack',
       description:
           'Nous avons créé notre nouvelle base de soin Semilac pour soutenir la régénération des ongles fragilisés, fins et cassants. Nous l avons enrichi de fibres de biotine, de kératine, de vitamine E et de nylon qui enveloppent et remplissent la plaque à ongles. En conséquence, une couche protectrice est créée sur sa surface, qui la protège contre les dommages mécaniques, la fissuration et la rupture. Il permet également la superstructure et l extension du clou jusqu à 10 mm. Grâce à sa consistance depaisseur moyenne, notre base est facile à appliquer.Son avantage est également la nuance transparente du rose pudding - il peut être utilisé comme base pour tous les vernis hybrides ou directement appliqué dessus avec un dessus, ce qui vous permet d obtenir l effet d une manucure naturelle.',
     ),
@@ -45,7 +45,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/16022113401kGqpRDy.png',
       price: 349,
-      category: 'Manucure',
+      category: 'Pack',
       description:
           'DÉCOUVREZ LE VERNIS HYBRIDE INNOVANT SEMILAC ONE STEP EN BOUTEILLE .Vous n avez jamais essayé une manucure hybride et vous rêvez de beaux ongles bien entretenus? Surtout pour vous, nous avons créé un produit avec lequel la  première manucure hybride  sera un jeu d enfant. ',
     ),
@@ -55,7 +55,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/1602893621ZYECYReF.png',
       price: 249,
-      category: 'Manucure',
+      category: 'vêtements',
       description:
           "ampe LED UV 36W blanche extrêmement élégante en forme de diamant avec technologie double LED qui combine deux types de lumière. Cette technologie permet à notre lampe de polymériser parfaitement tous les vernis hybrides et les gels Semilac. C'est un vrai diamant parmi les lampes!",
     ),
@@ -65,7 +65,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/1602211164sVH6N1Yk.png',
       price: 159,
-      category: 'Manucure',
+      category: 'vêtements',
       description:
           'Ampoule UV professionnelle de 9 W, utilisée dans les lampes pour le séchage des gels Semilac et des vernis hybrides.',
     ),
@@ -75,7 +75,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/1602194737KRkUL5J3.png',
       price: 230,
-      category: 'Manucure',
+      category: 'vêtements',
       description:
           "Vous vous demandez comment obtenir l'hybride? Nous répondons - avec Semilac! C'est encore plus facile maintenant! Si vous souhaitez retirer l'hybride de vos ongles à la maison, tout ce dont vous avez besoin est notre kit de suppression hybride Semilac unique ! Vous trouverez ici tous les produits nécessaires pour télécharger l'hybride et un dépliant expliquant ÉTAPE PAR ÉTAPE sur la façon de télécharger l'hybride.",
     ),
@@ -85,7 +85,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/1602892762qjcK7FaD.png',
       price: 149,
-      category: 'Maquillage',
+      category: 'Electronique',
       description:
           "Une palette unique de cinq fards à paupières aux tons chauds , parfaite pour le maquillage de jour et de soirée. Les ombres de la palette Special Day sont les couleurs or clair, violet et noir , avec différentes finitions: satiné, mat et brillant. Une bonne pigmentation des ombres vous permet de profiter longtemps de leur couleur, en soulignant magnifiquement l'œil. Grâce à la structure parfaite de toutes les couleurs, vous pouvez les appliquer de n'importe quelle manière - du bout du doigt ou avec un pinceau. ",
     ),
@@ -96,7 +96,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://planetdiscounts.ma/assets/images/products/1602196108nWaz5Hnv.png',
       price: 299,
-      category: 'Maquillage',
+      category: 'Electronique',
       description:
           "La teinte mate Semilac Natural Beige 401 est un beige clair et neutre dans une teinte de peau saine. Parfait pour unifier la couleur de la paupière, à appliquer comme couleur de base ou pour éclaircir le coin interne",
     ),
@@ -106,7 +106,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://semilac.pl/media/catalog/product/cache/c66786404a7109b68bb918f8905d342c/s/e/semilac_cie_do_powiek_w_kremie_pink_gold_094_3.jpg',
       price: 4,
-      category: 'Maquillage',
+      category: 'Electronique',
       description:
           "Ombre à paupières bijou en crème, correspondant à la couleur du vernis hybride Semilac 094 Or Rose. C'est une lueur délicatement rose enrichie d'éclairs d'or, qui fonctionnera bien dans un maquillage de jour plus visible et dans le cadre d'un maquillage de mise en valeur pour une soirée. La formule légère se caractérise par un séchage rapide sur la paupière, une durabilité exceptionnelle et la possibilité d'une combinaison illimitée avec les couleurs de base du fond de teint. L'éclat dichromatique plaira aux femmes qui apprécient l'élégance classique assaisonnée d'une pincée de féminité significative.",
     ),
@@ -116,7 +116,7 @@ class Products with ChangeNotifier {
       imgUrl:
           'https://semilac.pl/media/catalog/product/cache/c66786404a7109b68bb918f8905d342c/s/k/sklep_www_pomadki_klasyczne_005.jpg',
       price: 4,
-      category: 'Maquillage',
+      category: 'Electronique',
       description:
           " Classy Lips Berry Nude 005  est une excellente combinaison d'une couverture parfaite et d'une texture soyeuse. Le beige foncé avec une délicate touche de rouge rouillé semble phénoménal. Le rouge à lèvres est riche en émollients et pigments pour un fini satiné . L'enrichissement de la formule en vitamine E  améliore l'élasticité et protège la peau sensible des lèvres contre les effets néfastes des facteurs externes. Grâce à des pigments hautement saturés, les lèvres gagneront en définition tout en conservant une structure délicate et crémeuse.",
     ),
