c54a8e08c21c08e2af0e724063e54ec975bbb457
HAFDIAHMED <ahafdi62@gmail.com>
2020-11-12 13:10:39 +0100

    new changes

---
+++ lib/screens/liste_category.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../widgets/semilac.dart';
+
+class ListCategory extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xfffb5d04),
+          title: Text(
+            ' Catégories',
+            style:
+                TextStyle(fontSize: 30, color: Theme.of(context).accentColor),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.wb_sunny),
+                title: Text('Sun'),
+              ),
+              ListTile(
+                leading: Icon(Icons.brightness_3),
+                title: Text('Moon'),
+              ),
+              ListTile(
+                leading: Icon(Icons.star),
+                title: Text('Star'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
