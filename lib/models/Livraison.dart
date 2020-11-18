d12154304644cbea585fea1fe0a8ab290c671f65
HAFDIAHMED <ahafdi62@gmail.com>
2020-11-18 13:55:12 +0100

    new changes

--- lib/models/Livraison.dart
+++ lib/models/Livraison.dart
@@ -17,7 +17,7 @@ class Livraison extends StatelessWidget {
           //date: '1 June 2016',
           description:
               'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          imglink: 'assets/images/brand (2).jpg'),
+          imglink: 'assets/images/amana.png'),
     ];
 
     return Scaffold(
